31b1378c48a2a01ffb22472bdec1ce85371a78d0
vanthap <vanthap@dunwoody.edu>
2019-01-16 12:41:07 -0600

    Random commit to save my current work load

--- lab_one/lab_1.cs
+++ lab_one/lab_1.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class Lab_1
+namespace Lab_1
 {
-	public Lab_1()
-	{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring an array that holds correctAnswers
+            string correctAnswers = new string [10] { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" };
+
+            //An array for end-users selection
+            string userSelection = new string [10];
+
+            //Declare an array that holds the questions
+            string quizQuestions = new string[ 10]  { "Is .NET core open-source?", "Does .NET core support cross-platform scenarios?", "When was .NET core released?", "What is the current version of .NET core?", "How many cross-platform scenarios can .NET core support?" };
+
+            //Declare a Multi-dimensional array that holds options for each questions
+            string quizQuestionOptions = new string [ , ]  {
+                {"A) .NET core is not public yet", "B) Yes", "C) I don't know", "D) No"},
+                {"A) Need permission from Microsoft for cross-platform", "B) Yes", "C) No", "D) In current development"},
+                {"A) 2019", "B) 2003", "C) 2016", "D) 1990"},
+                {"A) 2.2", "B) 1.5", "C) 0.7", "D) 6.4"},
+                {"A) 2", "B) 1", "C) 0", "D) 4"},
+                {"AA) ", "B) ", "C) ", "D) "},
+                {"AA) ", "B) ", "C) ", "D) "},
+                {"A) ", "B) ", "CC) ", "D) "},
+                {"A) ", "B) ", "C) ", "DD) "},
+                {"AA) ", "B) ", "C) ", "D) "},
+            };
+
+            string selection;
+            num start
+            num EXIT = -1
 
 
 
 
-	}
+
+
+
+
+        }
+    }
 }
