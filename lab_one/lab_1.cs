e42f48ac6a3ba218dc96915be5ab835ebf1b5221
vanthap <vanthap@dunwoody.edu>
2019-01-17 12:24:16 -0600

    Var = String[] Not String Var = String[]

--- lab_one/lab_1.cs
+++ lab_one/lab_1.cs
@@ -8,16 +8,16 @@ namespace Lab_1
         static void Main(string[] args)
         {
             //Declaring an array that holds correctAnswers
-            string correctAnswers = new string[5] { "B", "B", "C", "A", "D" };
+            correctAnswers = new string[5] { "B", "B", "C", "A", "D" };
 
             //An array for end-users selection
-            string userSelection = new string[5];
+            userSelection = new string[5];
 
             //Declare an array that holds the questions
-            string quizQuestions = new string[5] { "Is .NET core open-source?", "Does .NET core support cross-platform scenarios?", "When was .NET core released?", "What is the current version of .NET core?", "How many cross-platform scenarios can .NET core support?" };
+            quizQuestions = new string[5] { "Is .NET core open-source?", "Does .NET core support cross-platform scenarios?", "When was .NET core released?", "What is the current version of .NET core?", "How many cross-platform scenarios can .NET core support?" };
 
             //Declare a Multi-dimensional array that holds options for each questions
-            string quizQuestionOptions = new string[,]  {
+            quizQuestionOptions = new string[,]  {
                 {"A) .NET core is not public yet", "B) Yes", "C) I don't know", "D) No"},
                 {"A) Need permission from Microsoft for cross-platform", "B) Yes", "C) No", "D) In current development"},
                 {"A) 2019", "B) 2003", "C) 2016", "D) 1990"},
