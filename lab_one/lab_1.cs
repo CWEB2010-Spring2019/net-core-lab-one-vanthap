b03cdc19cc3c41441689cfd1ed1766d56f5a89be
vanthap <vanthap@dunwoody.edu>
2019-01-17 12:06:48 -0600

    Testing codes

--- lab_one/lab_1.cs
+++ lab_one/lab_1.cs
@@ -8,16 +8,16 @@ namespace Lab_1
         static void Main(string[] args)
         {
             //Declaring an array that holds correctAnswers
-            string correctAnswers = new string [10] { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" };
+            string correctAnswers = new string[10] { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" };
 
             //An array for end-users selection
-            string userSelection = new string [10];
+            string userSelection = new string[10];
 
             //Declare an array that holds the questions
-            string quizQuestions = new string[ 10]  { "Is .NET core open-source?", "Does .NET core support cross-platform scenarios?", "When was .NET core released?", "What is the current version of .NET core?", "How many cross-platform scenarios can .NET core support?" };
+            string quizQuestions = new string[10] { "Is .NET core open-source?", "Does .NET core support cross-platform scenarios?", "When was .NET core released?", "What is the current version of .NET core?", "How many cross-platform scenarios can .NET core support?" };
 
             //Declare a Multi-dimensional array that holds options for each questions
-            string quizQuestionOptions = new string [ , ]  {
+            string quizQuestionOptions = new string[,]  {
                 {"A) .NET core is not public yet", "B) Yes", "C) I don't know", "D) No"},
                 {"A) Need permission from Microsoft for cross-platform", "B) Yes", "C) No", "D) In current development"},
                 {"A) 2019", "B) 2003", "C) 2016", "D) 1990"},
@@ -33,7 +33,7 @@ namespace Lab_1
             string selection;
             string num = start;
 
-            string num = EXIT [-1];     //Sentinel Value
+            string num = EXIT[-1];     //Sentinel Value
 
             List<string> correct = new List<string>();
             List<string> wrong = new List<string>();
@@ -43,43 +43,50 @@ namespace Lab_1
             userInput = Console.ReadLine();
             int x = Int32.Parse(Console.ReadLine());
 
-            while (start() != EXIT());
+            while (start() != EXIT()) ;
 
 
-            for (num x = 0; x < quizQuestions.Length; x++) {
+                for (num x = 0; x < quizQuestions.Length; x++) {
 
-                Console.WriteLine(quizQuestions[10]);
-                for (num i = 0; i < quizQuestionOptions.GetLength(0); i++) {
-                    Console.WriteLine(quizQuestionOptions[x, i])
-                    };
+                    Console.WriteLine(quizQuestions[10]);
+                    for (num i = 0; i < quizQuestionOptions.GetLength(0); i++) {
+                        Console.WriteLine(quizQuestionOptions[x, i])
+                        };
 
-                Console.WriteLine("Please enter an option");
-                userInput = selection();
-                userSelection[x] = selection;
+                    Console.WriteLine("Please enter an option");
+                    userInput = selection();
+                    userSelection[x] = selection;
 
-                //Need to determind if the user's selection is the correct answer
-                if (selection == correctAnswers[x]) { 
-                    correct.Add(selection) };
-			    else
-				    wrong.Add(selection);
-			   
-			
-			    string wrong.Clear;
-			    string correct.Clear;
+                    //Need to determind if the user's selection is the correct answer
+                    if (selection == correctAnswers[x]) {
+                        correct.Add(selection);
+			        else
+				        wrong.Add(selection); }
 
 
+                    string wrong.Clear
+                    string correct.Clear
+
+                }
+
+                Console.WriteLine("You answered " + correct.Count + " out of " + quizQuestionOptions.Length);
+
+                //Determind if they passed
+                var results = (correct.Count >= 1) ? "You passed the assesment" : "You didn't pass";
+
+                //OUTPUT the wrong questions
+                Console.WriteLine("Listed below are the questions you got wrong");
+                Console.WriteLine(wrong.ForEach(x => { x} ));
 
 
 
+                Console.WriteLine("To take the quiz again, enter any value other than -1. To exit enter -1");
+                INPUT start
 
+            //ENDWHILE
 
-
-
-                };
-
-
-
-
+            Console.WriteLine("Program has concluded");
         }
+
     }
 }
