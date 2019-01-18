df2a8dcd6e5d9756259b356e9be9c1b03b0a88f9
vanthap <vanthap@dunwoody.edu>
2019-01-17 18:37:32 -0600

    More debug and fixer upper

--- lab_one/lab_1.cs
+++ lab_one/lab_1.cs
@@ -37,67 +37,65 @@ namespace Lab_1
                 {"A) 2019", "B) 2003", "C) 2016", "D) 1990"},
                 {"A) 2.2", "B) 1.5", "C) 0.7", "D) 6.4"},
                 {"A) 2", "B) 1", "C) 0", "D) 4"},
-                {"A) ", "B) ", "C) ", "D) "},
-                {"A) ", "B) ", "C) ", "D) "},
-                {"A) ", "B) ", "C) ", "D) "},
-                {"A) ", "B) ", "C) ", "D) "},
-                {"A) ", "B) ", "C) ", "D) "}
+                {"AA) ", "B) ", "C) ", "D) "},
+                {"AA) ", "B) ", "C) ", "D) "},
+                {"A) ", "B) ", "CC) ", "D) "},
+                {"A) ", "B) ", "C) ", "DD) "},
+                {"AA) ", "B) ", "C) ", "D) "}
             };
 
             string selection;
             int start;
-            int EXIT;     //Sentinel Value
+            int EXIT = -1;     //Sentinel Value
 
+            //Lists to contain the correct selection and wrong selection
             List<string> correct = new List<string>();
             List<string> wrong = new List<string>();
 
-            Console.WriteLine("Welcome, this program will assess your knowledge of .NET core");
-            Console.WriteLine("To get started, please enter any key");
-            userInput = Console.ReadKey();
-            int x = Int32.Parse(Console.ReadKey());
+            //Calls in another method/function
+            start = greetings();
 
-            while (start() != EXIT()) ;
-
-
-                for (num x = 0; x < quizQuestions.Length; x++) {
+            //START While
+            while (start != EXIT)
+            {
+                for (num x = 0; x < quizQuestions.Length; x++)
+                {
 
                     Console.WriteLine(quizQuestions[10]);
-                    for (num i = 0; i < quizQuestionOptions.GetLength(0); i++) {
-                    Console.WriteLine(quizQuestionOptions[x, i]);
+                    for (num i = 0; i < quizQuestionOptions.GetLength(0); i++)
+                    {
+                        Console.WriteLine(quizQuestionOptions[x, i]);
                     }
 
-                    Console.WriteLine("Please enter an option");
-                    userInput = selection();
+                    Console.WriteLine("Please enter an option:");
+                    selection = ReadLine();
                     userSelection[x] = selection;
 
-                    //Need to determind if the user's selection is the correct answer
-                    if (selection == correctAnswers[x]) {
-                        correct.Add(selection); }
-                    else
-                        { 
-                        wrong.Add(selection); }
+                    //If statement to determind if the selection was correct or wrong
+                    if (selection == correctAnswers[x])
+                        correct.Add(selection);
 
+                    else
+                        wrong.Add("Question: " + (x + 1) + selection);
 
                     wrong.Clear;
                     correct.Clear;
-
                 }
+            }
+            //END While
 
+            //Output their score out of 10
             Console.WriteLine("You answered " + correct.Count + " out of " + quizQuestionOptions.Length);
 
             //Determind if they passed
             var results = (correct.Count >= 1) ? "You passed the assesment" : "You didn't pass";
 
-            //OUTPUT the wrong questions
+            //Output the questions they got wrong
             Console.WriteLine("Listed below are the questions you got wrong");
             wrong.ForEach(x => { Console.WriteLine(x); });
 
-
-
             Console.WriteLine("To take the quiz again, enter any value other than -1. To exit enter -1");
             start = ReadLine();
-
-            //ENDWHILE
 
             Console.WriteLine("Program has concluded");
         }
