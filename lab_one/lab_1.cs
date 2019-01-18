1b47a6b13e6e737e811342db7747eabbe0ec2ccd
vanthap <vanthap@dunwoody.edu>
2019-01-17 18:22:00 -0600

    More adjustment to make code easier to read.

--- lab_one/lab_1.cs
+++ lab_one/lab_1.cs
@@ -13,20 +13,35 @@ namespace Lab_1
             //Array to hold the correct answers. Answer key: B, B, C, A, D, A, A, C, D, A
             string[] correctAnswers = new string[10] { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" };
 
-            //An array for end-users selection
-            string[] userSelection = new string[5];
+            //Array to hold user's input and selection choice
+            string[] userSelection = new string[10];
 
-            //Declare an array that holds the questions
-            string[] quizQuestions = new string[5] { "Is .NET core open-source?", "Does .NET core support cross-platform scenarios?", "When was .NET core released?", "What is the current version of .NET core?", "How many cross-platform scenarios can .NET core support?" };
+            //Array to hold the question for the quiz
+            string[] quizQuestions = new string[10] {
+                "Is .NET core open-source?",
+                "Does .NET core support cross-platform scenarios?",
+                "When was .NET core released?",
+                "What is the current version of .NET core?",
+                "How many cross-platform scenarios can .NET core support?",
+                "",
+                "",
+                "",
+                "",
+                ""
+            };
 
-            //Declare a Multi-dimensional array that holds options for each questions
+            //Multi-dimensional array that holds options for each questions
             string[,] quizQuestionOptions =  {
                 {"A) .NET core is not public yet", "B) Yes", "C) I don't know", "D) No"},
                 {"A) Need permission from Microsoft for cross-platform", "B) Yes", "C) No", "D) In current development"},
                 {"A) 2019", "B) 2003", "C) 2016", "D) 1990"},
                 {"A) 2.2", "B) 1.5", "C) 0.7", "D) 6.4"},
-                {"A) 2", "B) 1", "C) 0", "D) 4"}
-                
+                {"A) 2", "B) 1", "C) 0", "D) 4"},
+                {"A) ", "B) ", "C) ", "D) "},
+                {"A) ", "B) ", "C) ", "D) "},
+                {"A) ", "B) ", "C) ", "D) "},
+                {"A) ", "B) ", "C) ", "D) "},
+                {"A) ", "B) ", "C) ", "D) "}
             };
 
             string selection;
